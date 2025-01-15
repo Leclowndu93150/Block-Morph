27cf816be85969c44f3401f69a3e61fbbf998751
Leclowndu93150 <xdr.a123454321@gmail.com>
2025-01-15 08:03:27 +0100

    fix lighting issues

--- forge/src/main/java/tfar/blockmorph/client/ModClientForge.java
+++ forge/src/main/java/tfar/blockmorph/client/ModClientForge.java
@@ -1,16 +1,25 @@
 package tfar.blockmorph.client;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.block.BlockRenderDispatcher;
 import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.client.event.RenderPlayerEvent;
+import net.minecraftforge.client.model.data.ModelData;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -42,16 +51,27 @@ public class ModClientForge {
         Player player = event.getEntity();
         PlayerDuck playerDuck = PlayerDuck.of(player);
         ItemStack stack = player.getItemBySlot(EquipmentSlot.HEAD);
-        if (!stack.isEmpty() &&playerDuck.isMorphed()) {
-            ItemRenderer itemRenderer = Minecraft.getInstance().getItemRenderer();
-            PoseStack pPoseStack = event.getPoseStack();
-            pPoseStack.pushPose();
-            MultiBufferSource pBuffer = event.getMultiBufferSource();
+        if (!stack.isEmpty() && playerDuck.isMorphed()) {
+            PoseStack poseStack = event.getPoseStack();
+            poseStack.pushPose();
+
+            BlockState state = Block.byItem(stack.getItem()).defaultBlockState();
+            BlockRenderDispatcher blockRenderer = Minecraft.getInstance().getBlockRenderer();
+            BakedModel model = blockRenderer.getBlockModel(state);
+
+            MultiBufferSource.BufferSource bufferSource = Minecraft.getInstance().renderBuffers().bufferSource();
+            VertexConsumer buffer = bufferSource.getBuffer(RenderType.solid());
+
             float scale = 2;
-            pPoseStack.translate(0,.5,0);
-            pPoseStack.scale(scale,scale,scale);
-            itemRenderer.renderStatic(stack, ItemDisplayContext.FIXED, event.getPackedLight(), OverlayTexture.NO_OVERLAY, pPoseStack, pBuffer, player.level(), player.getId());
-            pPoseStack.popPose();
+            poseStack.translate(-0.5, 0, -0.5);
+            //poseStack.scale(scale, scale, scale);
+
+            blockRenderer.renderBatched(state, player.blockPosition(), player.level(),
+                    poseStack, buffer, true, RandomSource.create(),
+                    ModelData.EMPTY, null);
+
+            bufferSource.endBatch(RenderType.solid());
+            poseStack.popPose();
             event.setCanceled(true);
         }
     }
